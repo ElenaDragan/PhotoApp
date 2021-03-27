e9898a15dc23fe651da2a6e7998ab3b362d4387b
Elena Dragan <elena.dragan264@gmail.com>
2021-03-27 12:01:24 +0200

    upload photo

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const multer = require('multer');
+const port = process.env.PORT || 3000;
 
 const errorController = require('./controllers/error');
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const blogRoutes = require('./routes/blog');
+const helpers = require('./helpers');
+app.post('/upload-profile-pic', (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
+
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
